074246c6a530c1e372f8c41411f9ec57335ec0a6
haekalsyakuro <89882123+haekalsyakuro@users.noreply.github.com>
2023-04-09 23:13:29 +0700

    Add files via upload

---
+++ laptop1/layout/navbar-admin.php
@@ -0,0 +1,5 @@
+<nav class="navbar navbar-expand-lg bg-white">
+  <div class="container-fluid">
+    <a class="navbar-brand" onclick="toggleSidebar()"><i class="fa-solid fa-bars"></i></a>
+  </div>
+</nav>
